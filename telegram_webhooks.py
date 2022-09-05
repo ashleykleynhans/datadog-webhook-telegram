7d7f1c58a51f4a52759b3a23f6dac59f93bb88ca
Ashley Kleynhans <ashley.kleynhans@gmail.com>
2022-09-05 09:17:33 +0200

    Improved error handling and sone refactoring

--- telegram_webhooks.py
+++ telegram_webhooks.py
@@ -6,7 +6,7 @@ import argparse
 import json
 import requests
 import time
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response
 from flask_basicauth import BasicAuth
 
 app = Flask(__name__)
@@ -18,29 +18,42 @@ telegram_warning_chat_id = os.environ['TELEGRAM_WARNING_CHAT_ID']
 telegram_critical_chat_id = os.environ['TELEGRAM_CRITICAL_CHAT_ID']
 
 
-@app.route('/')
-def ping():
-    return jsonify({"status": "ok"})
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='Telegram Datadog Webhook Receiver'
+    )
+
+    parser.add_argument(
+        '-p', '--port',
+        help='Port to listen on',
+        type=int,
+        default=8090
+    )
+
+    parser.add_argument(
+        '-H', '--host',
+        help='Host to bind to',
+        default='0.0.0.0'
+    )
+
+    return parser.parse_args()
 
 
-@app.route('/', methods =  ['POST'])
-@basic_auth.required
-def webhookHandler():
-    content = request.get_json()
-    alert_priority = content['alert_priority']
-    alert_query = content['alert_query']
-    alert_status = content['alert_status']
-    alert_title = content['alert_title']
-    alert_transition = content['alert_transition']
-    alert_type = content['alert_type']
-    event_title = content['event_title']
-    event_type= content['event_type']
-    link = content['link']
-    priority = content['priority']
-    snapshot = content['snapshot']
-    text_only_msg = content['text_only_msg']
-    tags = content['tags']
-    user = content['user']
+def send_telegram_notification(datadog_payload):
+    alert_priority = datadog_payload['alert_priority']
+    alert_query = datadog_payload['alert_query']
+    alert_status = datadog_payload['alert_status']
+    alert_title = datadog_payload['alert_title']
+    alert_transition = datadog_payload['alert_transition']
+    alert_type = datadog_payload['alert_type']
+    event_title = datadog_payload['event_title']
+    event_type= datadog_payload['event_type']
+    link = datadog_payload['link']
+    priority = datadog_payload['priority']
+    snapshot = datadog_payload['snapshot']
+    text_only_msg = datadog_payload['text_only_msg']
+    tags = datadog_payload['tags']
+    user = datadog_payload['user']
 
     if alert_type == 'error':
         chat_id = telegram_critical_chat_id
@@ -50,7 +63,16 @@ def webhookHandler():
     msg_txt = f'<b>{html.escape(event_title)}</b>\n\n'
     msg_txt += f'{html.escape(alert_status)}'
 
-    reply_markup = {'inline_keyboard': [[{'text' : 'Check Event','url' : link}]]}
+    reply_markup = {
+        'inline_keyboard': [
+            [
+                {
+                    'text': 'Check Event',
+                    'url': link
+                }
+            ]
+        ]
+    }
 
     msg_data = {
         'chat_id': chat_id,
@@ -75,14 +97,59 @@ def webhookHandler():
 
     bot_url = f'https://api.telegram.org/bot{telegram_bot_token}/{bot_endpoint}'
     req = requests.post(url=bot_url, data=msg_data, files=files)
-    telegram_response = req.json()
+    return req.json()
+
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify(
+        {
+            'status': 'error',
+            'msg': f'{request.url} not found',
+            'detail': str(error)
+        }
+    ), 404)
+
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return make_response(jsonify(
+        {
+            'status': 'error',
+            'msg': 'Internal Server Error',
+            'detail': str(error)
+        }
+    ), 500)
+
+
+@app.route('/')
+def ping():
+    return make_response(jsonify(
+        {
+            'status': 'ok'
+        }
+    ), 200)
+
+
+@app.route('/', methods=['POST'])
+@basic_auth.required
+def webhook_andler():
+    datadog_payload = request.get_json()
+    telegram_response = send_telegram_notification(datadog_payload)
+
+    if not telegram_response['ok']:
+        return make_response(jsonify(
+            {
+                'status': 'error',
+                'msg': f'Failed to send Telegram notification to chat id: {telegram_chat_id}',
+                'detail': telegram_response
+            }
+        ), 500)
 
     return jsonify(telegram_response)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Telegram Datadog Webhook Receiver')
-    parser.add_argument('-p','--port',help='listening port',type=int, default=8090)
-    parser.add_argument('-H','--host',help='listening host',default='0.0.0.0')
-    results = parser.parse_args()
-    app.run(host=results.host, port=results.port)
+    args = get_args()
+    app.run(host=args.host, port=args.port)
